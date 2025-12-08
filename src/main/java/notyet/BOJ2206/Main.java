1c95c8ece0dfc0038a6b703660f75089b1acf333
= <tjalswhd1@naver.com>
2025-12-08 17:58:50 +0900

    25.12.08 문제풀이 업로드

--- src/main/java/notyet/BOJ2206/Main.java
+++ src/main/java/notyet/BOJ2206/Main.java
@@ -8,17 +8,12 @@ public class Main {
     static int N;               // 지도의 세로 길이
     static int M;               // 지도의 가로 길이
     static int[][] map;
-    static boolean[][] visited;
     static int[][] move = {{1,0},{-1,0},{0,1},{0,-1}};
-    static int minDistance;
-    static List<Location> wall = new ArrayList<Location>();
-
+    static int answer;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-
-        st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         map = new int[N][M];
@@ -26,71 +21,96 @@ public class Main {
             String[] arr = br.readLine().split("");
             for(int j=0; j<M; j++){
                 map[i][j] = Integer.parseInt(arr[j]);
-                if(map[i][j] == 1){
-                    wall.add(new Location(i,j));
-                    map[i][j] = -1;
-                }
             }
         }
-        visited = new boolean[N][M];
-
-        // 당신은 (1, 1)에서 (N, M)의 위치까지 이동하려 하는데, 이때 최단 경로로 이동하려 한다.
-        // 최단경로는 맵에서 가장 적은 개수의 칸을 지나는 경로를 말하는데, 이때 시작하는 칸과 끝나는 칸도 포함해서 센다.
-        // 불가능할 때는 -1을 출력한다
-
-        minDistance = Integer.MAX_VALUE;
-
-        for(int w=0; w<wall.size(); w++){
-            Location loc = wall.get(w);
-            int x = loc.x;
-            int y = loc.y;
-            map[x][y] = 0;                          // 벽 1개 부수기
-            visited = new boolean[N][M];
-            bfs(0,0);
-
-            if(map[N-1][M-1] != 0){                                         // 0이라면 BFS가 이루어지지 않음
-                minDistance = Math.min(minDistance, map[N-1][M-1]);         // BFS가 이루어진 후에 거리 반환
-            }
-            map[x][y] = -1;                          // 벽 복원
+        int[][][] distance = new int[N][M][2];
+        distance[N-1][M-1][0] = 1000 * 1000;
+        distance[N-1][M-1][1] = 1000 * 1000;
+        boolean[][] visited = new boolean[N][M];
+        boolean[][] breakVisited = new boolean[N][M];
+        BFS(new Location(0, 0), distance, visited, breakVisited);
+        if(distance[N-1][M-1][0] < distance[N-1][M-1][1]){
+            answer = distance[N-1][M-1][0];
         }
-        if(minDistance == Integer.MAX_VALUE){
+        answer = Math.min(distance[N-1][M-1][0], distance[N-1][M-1][1]);
+        if(answer == 1000 * 1000){
             bw.write(-1 + "\n");
         } else {
-            bw.write(minDistance + "\n");
+            bw.write(answer + "\n");
         }
         bw.flush();
     }
 
-    public static void bfs(int x, int y){               // (x,y)에서 출발하는 BFS
+    public static void BFS(Location loc, int[][][] distance, boolean[][] visited, boolean[][] breakVisited){
+        int r = loc.r;
+        int c = loc.c;
         Queue<Location> queue = new LinkedList<>();
-        queue.add(new Location(x, y));
-        visited[x][y] = true;
-        map[x][y] = 1;
+        Queue<Boolean> breakQueue = new LinkedList<>();
+        Queue<Integer> distanceQueue = new LinkedList<>();
+        queue.offer(loc);
+        breakQueue.offer(false);
+        distanceQueue.offer(1);
+        visited[r][c] = true;
+        distance[r][c][0] = 1;
 
-        while(!queue.isEmpty()){               // N,M 좌표에 도달하면 즉시 중단
-            Location loc = queue.poll();
+        while(!queue.isEmpty()){
+            loc = queue.poll();
+            boolean wallBreak = breakQueue.poll();
+            int dist = distanceQueue.poll();
+            r = loc.r;
+            c = loc.c;
             for(int i=0; i<4; i++){
-                int nx = loc.x + move[i][0];
-                int ny = loc.y + move[i][1];
-                if(checkRange(nx, ny) && !visited[nx][ny] && map[nx][ny] != -1){
-                    queue.add(new Location(nx, ny));
-                    visited[nx][ny] = true;
-                    map[nx][ny] = map[loc.x][loc.y] + 1;
+                int nr = r + move[i][0];
+                int nc = c + move[i][1];
+                if(checkRange(nr, nc)){
+                    if(!wallBreak){
+                        if(map[nr][nc] == 0){
+                            if(visited[nr][nc]){
+                                continue;
+                            } else {
+                                distance[nr][nc][0] = dist + 1;
+                                visited[nr][nc] = true;
+                                queue.offer(new Location(nr, nc));
+                                breakQueue.offer(false);
+                                distanceQueue.offer(dist + 1);
+                            }
+                        } else {
+                            distance[nr][nc][1] = dist + 1;
+                            breakVisited[nr][nc] = true;
+                            queue.offer(new Location(nr, nc));
+                            breakQueue.offer(true);
+                            distanceQueue.offer(dist + 1);
+                        }
+                    } else {
+                        if(map[nr][nc] == 1){
+                            continue;
+                        } else {
+                            if(breakVisited[nr][nc]){
+                                continue;
+                            } else {
+                                distance[nr][nc][1] = dist + 1;
+                                breakVisited[nr][nc] = true;
+                                queue.offer(new Location(nr, nc));
+                                breakQueue.offer(true);
+                                distanceQueue.offer(dist + 1);
+                            }
+                        }
+                    }
                 }
             }
         }
     }
 
     static class Location{
-        int x;
-        int y;
-        public Location(int x, int y){
-            this.x = x;
-            this.y = y;
+        int r;
+        int c;
+        public Location(int r, int c){
+            this.r = r;
+            this.c = c;
         }
     }
 
-    public static boolean checkRange(int x, int y){
-        return (x >= 0 && x < N && y >= 0 && y < M);
+    public static boolean checkRange(int r, int c){
+        return r >= 0 && r < N && c >= 0 && c < M;
     }
 }
