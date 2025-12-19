62e3de61291d9453ce7ba0ec7b4018971d4c48b0
Seo Min Jong <tjalswhd1@naver.com>
2025-12-20 01:01:22 +0900

    [Silver IV] Title: 카드2, Time: 124 ms, Memory: 43520 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    static int answer;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=1; i<=N; i++){
+            queue.offer(i);
+        }
+        while(true){
+            int num = queue.poll();
+            if(queue.isEmpty()){
+                answer = num;
+                break;
+            }
+            queue.offer(queue.poll());
+        }
+        bw.write(answer + "\n");
+        bw.flush();
+    }
+}
