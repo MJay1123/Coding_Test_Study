1d0631c5a498a496597ab5bb53277e62a5a2a4d1
Seo Min Jong <tjalswhd1@naver.com>
2025-12-20 00:55:28 +0900

    [Bronze V] Title: A/B, Time: 124 ms, Memory: 16112 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,21 @@
+
+
+import java.io.*;
+import java.nio.Buffer;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        double result = (double)A/B;
+        bw.write(result + "\n");
+        bw.flush();
+    }
+}
