d18fa05ec6188fddf8c98eb8765c748ab2465acd
Seo Min Jong <tjalswhd1@naver.com>
2025-12-28 01:17:22 +0900

    [level 2] Title: 소수 찾기, Time: 63.17 ms, Memory: 89.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,53 @@
+import java.util.*;
+class Solution {
+    static HashSet<Integer> hs = new HashSet<>();
+    static boolean[] isPrime = new boolean[9999999+1];
+    public int solution(String numbers) {
+        isPrime[0] = true;
+        isPrime[1] = true;
+        for(int i=2; i<=Math.sqrt(9999999+1); i++){
+            if(!isPrime[i]){
+                for(int j=2; i*j<isPrime.length; j++){
+                    isPrime[i*j] = true;
+                }
+            }
+        }
+        int[] numberArr = new int[numbers.length()];
+        for(int i=0; i<numbers.length(); i++){
+            numberArr[i] = numbers.charAt(i) - '0';
+        }
+        Arrays.sort(numberArr);
+        for(int depth=1; depth<=numberArr.length; depth++){
+            boolean[] visited = new boolean[numberArr.length];
+            List<Integer> list = new ArrayList<>();
+            getNumber(numberArr, visited, list, depth);
+        }
+        System.out.println(hs);
+        int answer = hs.size();
+        return answer;
+    }
+    public void getNumber(int[] numberArr, boolean[] visited, List<Integer> list, int depth){
+        if(list.size() == depth){
+            // System.out.println("success");
+            // System.out.println(depth + ", " + list);
+            int result = 0;
+            for(int i=0; i<list.size(); i++){
+                result += Math.pow(10, i) * list.get(list.size()-1-i);
+            }
+            if(!isPrime[result]){
+                hs.add(result);
+            }
+            return;
+        }
+        for(int i=0; i<numberArr.length; i++){
+            if(visited[i]){
+                continue;
+            }
+            visited[i] = true;
+            list.add(numberArr[i]);
+            getNumber(numberArr, visited, list, depth);
+            visited[i] = false;
+            list.remove(list.size()-1);
+        }
+    }
+}
