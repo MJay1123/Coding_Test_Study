6a66070ec7920aedbc69a6fdae3df59041e0e90a
Seo Min Jong <tjalswhd1@naver.com>
2025-12-28 02:19:44 +0900

    Refactor prime number detection logic
    Refactor prime number array initialization and sieve logic.

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,11 +1,12 @@
 import java.util.*;
 class Solution {
     static HashSet<Integer> hs = new HashSet<>();
-    static boolean[] isPrime = new boolean[9999999+1];
+    static boolean[] isPrime;
     public int solution(String numbers) {
+        isPrime = new boolean[(int)Math.pow(10, numbers.length())];
         isPrime[0] = true;
         isPrime[1] = true;
-        for(int i=2; i<=Math.sqrt(9999999+1); i++){
+        for(int i=2; i<=Math.sqrt(isPrime.length); i++){
             if(!isPrime[i]){
                 for(int j=2; i*j<isPrime.length; j++){
                     isPrime[i*j] = true;
@@ -28,8 +29,6 @@ class Solution {
     }
     public void getNumber(int[] numberArr, boolean[] visited, List<Integer> list, int depth){
         if(list.size() == depth){
-            // System.out.println("success");
-            // System.out.println(depth + ", " + list);
             int result = 0;
             for(int i=0; i<list.size(); i++){
                 result += Math.pow(10, i) * list.get(list.size()-1-i);
