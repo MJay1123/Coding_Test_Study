1f51600488b3fa46b87a3c48daf0a9349011578a
Seo Min Jong <tjalswhd1@naver.com>
2026-01-07 23:51:26 +0900

    [D1] Title: 홀수만 더하기, Time: 94 ms, Memory: 24,960 KB -BaekjoonHub

---
+++ SWEA/D1/2072. 홀수만 더하기/홀수만 더하기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+	public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int T = Integer.parseInt(br.readLine());
+		for(int test_case = 1; test_case <= T; test_case++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+            int sum = 0;
+            for(int i=0; i<10; i++){
+                int num = Integer.parseInt(st.nextToken());
+                if(num % 2 == 1){
+                    sum += num;
+                }
+            }
+            bw.write("#" + test_case + " " + sum + "\n");
+		}
+        bw.flush();
+	}
+}
