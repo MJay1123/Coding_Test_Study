f41fd2406b3b1ff11343f4685b24131735ee7ec2
Seo Min Jong <tjalswhd1@naver.com>
2026-01-14 09:25:04 +0900

    [D2] Title: 지그재그 숫자, Time: 79 ms, Memory: 24,960 KB -BaekjoonHub

---
+++ SWEA/D2/1986. 지그재그 숫자/README.md
@@ -0,0 +1,15 @@
+# [D2] 지그재그 숫자 - 1986 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PxmBqAe8DFAUq) 
+
+### 성능 요약
+
+메모리: 24,960 KB, 시간: 79 ms, 코드길이: 742 Bytes
+
+### 제출 일자
+
+2026-01-14 09:24
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D2/1986. 지그재그 숫자/지그재그 숫자.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+	public static void main(String args[]) throws Exception	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int T = Integer.parseInt(br.readLine());
+		for(int test_case = 1; test_case <= T; test_case++) {
+            int N = Integer.parseInt(br.readLine());
+            int answer = 0;
+            for(int i=1; i<=N; i++){
+                if(i % 2 == 1){
+                    answer += i;
+                } else {
+                    answer -= i;
+                }
+            }
+            bw.write("#" + test_case + " " + answer + "\n");
+		}
+        bw.flush();
+	}
+}
