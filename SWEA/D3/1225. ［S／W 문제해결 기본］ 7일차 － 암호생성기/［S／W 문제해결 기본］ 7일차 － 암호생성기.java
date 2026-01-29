7c53ad4eb9a918dc77367ddf90f312c9847ceb2b
Seo Min Jong <tjalswhd1@naver.com>
2026-01-29 13:53:50 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 115 ms, Memory: 33,024 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -1,45 +1,37 @@
+import java.util.*;
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
 
-public class Solution {
-    static Queue<Integer> queue;
-
-    public static void main(String[] args) throws IOException {
+class Solution {
+	public static void main(String args[]) throws Exception	{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-
-        for(int t=1; t<=10; t++){
-            int testNumber = Integer.parseInt(br.readLine());
-            st = new StringTokenizer(br.readLine());
-            queue = new LinkedList<>();
-            for(int i=0; i<8; i++){
-                int input = Integer.parseInt(st.nextToken());
-                queue.add(input);
-            }
-            boolean repeat = true;
-            while(repeat){
-                for(int i=1; i<=5; i++){
-                    int num = queue.poll();
-                    num -= i;
-                    if(num <= 0){
-                        num = 0;
-                        queue.offer(num);
-                        repeat = false;
-                        break;
-                    } else {
-                        queue.offer(num);
-                    }
-                }
-            }
-            bw.write("#" + testNumber + " ");
-            for(int i=0; i<8; i++){
-                bw.write(queue.poll() + " ");
-            }
-            bw.write("\n");
-        }
+        StringBuilder sb = new StringBuilder();
+		for(int test_case = 1; test_case <= 10; test_case++) {
+			int testCase = Integer.parseInt(br.readLine());
+			sb.append("#").append(test_case).append(" ");
+			Queue<Integer> queue = new LinkedList<>();
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int i=0; i<8; i++) {
+				queue.offer(Integer.parseInt(st.nextToken()));
+			}
+			int minusNum = 5;
+			while(true) {
+				minusNum = minusNum % 5 + 1;
+				int number = queue.poll();
+				if(number - minusNum <= 0) {
+					number = 0;
+					queue.offer(number);
+					break;
+				}
+				number -= minusNum;
+				queue.offer(number);
+			}
+			while(!queue.isEmpty()) {
+				sb.append(queue.poll()).append(" ");
+			}
+            sb.append("\n");
+		}
+		bw.write(sb.toString());
         bw.flush();
-    }
+	}
 }
