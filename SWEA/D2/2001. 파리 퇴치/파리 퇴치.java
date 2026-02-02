9ad2766600db2048af64f4ad9ec238b6a5558b9e
Seo Min Jong <tjalswhd1@naver.com>
2026-02-02 14:11:27 +0900

    [D2] Title: 파리 퇴치, Time: 78 ms, Memory: 22,528 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -1,39 +1,42 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 class Solution {
-	public static void main(String args[]) throws Exception	{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
-		for(int test_case = 1; test_case <= T; test_case++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            int M = Integer.parseInt(st.nextToken());
-            int[][] map = new int[N][N];
-            for(int r=0; r<N; r++){
-                st = new StringTokenizer(br.readLine());
-                for(int c=0; c<N; c++){
-                    map[r][c] = Integer.parseInt(st.nextToken());
-                }
-            }
-            int answer = 0;
-            for(int r=0; r<=N-M; r++){
-                for(int c=0; c<=N-M; c++){
-                    answer = Math.max(answer, catchFly(map, r, c, M));
-                }
-            }
-            bw.write("#" + test_case + " " + answer + "\n");
+		for (int test_case = 1; test_case <= T; test_case++) {
+			sb.append("#").append(test_case).append(" ");
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			int[][] map = new int[N][N];
+			for(int r=0; r<N; r++) {
+				st = new StringTokenizer(br.readLine());
+				for(int c=0; c<N; c++) {
+					map[r][c] = Integer.parseInt(st.nextToken());
+				}
+			}
+			int answer = 0;
+			for(int r=0; r<N-M+1; r++) {
+				for(int c=0; c<N-M+1; c++) {
+					int sum = 0;
+					for(int i=0; i<M; i++) {
+						for(int j=0; j<M; j++) {
+							sum += map[r+i][c+j];
+						}
+					}
+					answer = Math.max(answer,  sum);
+				}
+			}
+			sb.append(answer).append("\n");
 		}
-        bw.flush();
+		bw.write(sb.toString());
+		bw.flush();
 	}
-    public static int catchFly(int[][] map, int r, int c, int M){
-        int count = 0;
-        for(int i=0; i<M; i++){
-            for(int j=0; j<M; j++){
-                count += map[r+i][c+j];
-            }
-        }
-        return count;
-    }
 }
