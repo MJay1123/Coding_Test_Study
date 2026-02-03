edef135d3a5dc5cde79a279b67998f893efd0eb7
Seo Min Jong <tjalswhd1@naver.com>
2026-02-03 16:40:37 +0900

    [D3] Title: 한빈이와 Spot Mart, Time: 112 ms, Memory: 30,336 KB -BaekjoonHub

---
+++ SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		int TC = Integer.parseInt(br.readLine());
+		for (int testCase = 1; testCase <= TC; testCase++) {
+			sb.append("#").append(testCase).append(" ");
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());	// 과자 봉지 개수
+			int M = Integer.parseInt(st.nextToken());	// 무게 합 제한
+			st = new StringTokenizer(br.readLine());
+			int[] snacks = new int[N];
+			for(int i=0; i<N; i++) {
+				snacks[i] = Integer.parseInt(st.nextToken());
+			}
+			Arrays.sort(snacks);
+			int left = 0;
+			int right = N-1;
+			int sum = 0;
+			int answer = -1;
+			while(left < right) {
+				sum = snacks[left] + snacks[right];
+				if(sum <= M) {
+					answer = Math.max(answer,  sum);
+					left++;
+				} else {
+					right--;
+				}
+			}
+			sb.append(answer).append("\n");
+		}
+		bw.write(sb.toString());
+		bw.flush();
+	}
+}
