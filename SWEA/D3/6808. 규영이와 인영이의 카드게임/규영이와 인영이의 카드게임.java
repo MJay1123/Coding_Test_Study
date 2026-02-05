cf37a4af9626d1b207fdf1047fa6595edd7eb464
Seo Min Jong <tjalswhd1@naver.com>
2026-02-05 11:39:37 +0900

    [D3] Title: 규영이와 인영이의 카드게임, Time: 2,586 ms, Memory: 27,008 KB -BaekjoonHub

--- SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
+++ SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
@@ -3,9 +3,6 @@ import java.io.*;
 
 class Solution {
 	static int[] answer = new int[2];
-	static boolean[] visited;
-	static int[] arrA;
-	static int[] arrB;
 	public static void main(String args[]) throws Exception	{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -14,21 +11,21 @@ class Solution {
 		for(int test_case = 1; test_case <= T; test_case++) {
 			sb.append("#").append(test_case).append(" ");
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			arrA = new int[9];
-			visited = new boolean[19];
+			int[] arrA = new int[9];
+			boolean[] visited = new boolean[19];
 			for(int i=0; i<9; i++) {
 				arrA[i] = Integer.parseInt(st.nextToken());
 				visited[arrA[i]] = true;
 			}
-			arrB = new int[9];
+			int[] arrB = new int[9];
 			answer = new int[2];
-			P(0);
+			P(0, visited, arrA, arrB);
             sb.append(answer[0]).append(" ").append(answer[1]).append("\n");
 		}
 		bw.write(sb.toString());
         bw.flush();
 	}
-	public static void P(int index) {
+	public static void P(int index, boolean[] visited, int[] arrA, int[] arrB) {
 		if(index == 9) {
 			int reA = 0;
 			int reB = 0;
@@ -50,7 +47,7 @@ class Solution {
 			if(!visited[i]) {
 				visited[i] = true;
 				arrB[index] = i;
-				P(index+1);
+				P(index+1, visited, arrA, arrB);
 				visited[i] = false;
 			}
 		}
