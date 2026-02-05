0ba0e7c24356c26c3e5651edce69d7ec23248f21
Seo Min Jong <tjalswhd1@naver.com>
2026-02-05 11:05:19 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - String, Time: 82 ms, Memory: 24,704 KB -BaekjoonHub

--- SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
+++ SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
@@ -2,36 +2,29 @@ import java.util.*;
 import java.io.*;
 
 class Solution {
-	public static void main(String args[]) throws Exception	{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-		for(int test_case = 1; test_case <= 10; test_case++) {
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		for (int test_case = 1; test_case <= 10; test_case++) {
 			sb.append("#").append(test_case).append(" ");
 			br.readLine();
 			String target = br.readLine();
 			String str = br.readLine();
-			int answer = 0;
-			for(int i=0; i<str.length(); i++) {
-				if(str.charAt(i) == target.charAt(0) && exists(str, target, i)) {
-					answer++;
+			boolean atEnd = true;
+			for(int i=0; i<target.length(); i++) {
+				if(target.charAt(i) != str.charAt(str.length()-target.length()+i)) {
+					atEnd = false;
+					break;
 				}
 			}
-            sb.append(answer).append("\n");           
+			int answer = str.split(target).length - 1;
+			if(atEnd) {
+				answer++;
+			}
+			sb.append(answer).append("\n");
 		}
 		bw.write(sb.toString());
-        bw.flush();
-	}
-
-	public static boolean exists(String str, String target, int index) {
-		for (int i = 0; i < target.length(); i++) {
-			if (index + i >= str.length()) {
-				return false;
-			}
-			if (target.charAt(i) != str.charAt(index + i)) {
-				return false;
-			}
-		}
-		return true;
+		bw.flush();
 	}
 }
