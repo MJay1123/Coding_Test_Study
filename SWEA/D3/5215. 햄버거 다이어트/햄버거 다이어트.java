332ca6e0a01af0110eaed0e2cd7066787100c76d
Seo Min Jong <tjalswhd1@naver.com>
2026-02-06 10:38:08 +0900

    [D3] Title: 햄버거 다이어트, Time: 157 ms, Memory: 26,240 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,60 +1,45 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Solution {
-	public static void main(String args[]) throws Exception	{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int N, L;
+	static int answer;
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
-		for(int test_case = 1; test_case <= T; test_case++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            int L = Integer.parseInt(st.nextToken());
-            int[][] ingredients = new int[N][2];
-            List<Ingredient> list = new ArrayList<>();
-            for(int i=0; i<N; i++){
-                st = new StringTokenizer(br.readLine());
-                int score = Integer.parseInt(st.nextToken());
-                int calorie = Integer.parseInt(st.nextToken());
-                list.add(new Ingredient(score, calorie));
-            }
-            Collections.sort(list);
-            
-            int answer = choose(list, 0, 0, 0, L);
-			StringBuilder sb = new StringBuilder();
-            sb.append("#").append(test_case).append(" ").append(answer).append("\n");
-            bw.write(sb.toString());
+		for (int test_case = 1; test_case <= T; test_case++) {
+			sb.append("#").append(test_case).append(" ");
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			L = Integer.parseInt(st.nextToken());
+			int[][] ingredients = new int[N][2];
+			for(int i=0; i<N; i++) {
+				st = new StringTokenizer(br.readLine());
+				ingredients[i][0] = Integer.parseInt(st.nextToken());
+				ingredients[i][1] = Integer.parseInt(st.nextToken());
+			}
+			answer = 0;
+			boolean[] used = new boolean[N];
+			C(0, 0, 0, ingredients, used);
+			sb.append(answer).append(" ");
+			sb.append("\n");
 		}
-        bw.flush();
+		bw.write(sb.toString());
+		bw.flush();
 	}
-    public static int choose(List<Ingredient> list, int score, int calorie, int index, int L){
-        if(calorie > L){
-            return -1;
-        }
-        if(index == list.size()){
-            return score;
-        }
-        Ingredient i = list.get(index);
-        return Math.max(choose(list, score+i.score, calorie+i.calorie, index+1, L), choose(list, score, calorie, index+1, L));
-    }
-    static class Ingredient implements Comparable<Ingredient>{
-        int score;
-        int calorie;
-        public Ingredient(int score, int calorie){
-            this.score = score;
-            this.calorie = calorie;
-        }
-        
-        @Override
-        public int compareTo(Ingredient i){
-            if(this.calorie != i.calorie){
-                return this.calorie - i.calorie;
-            }
-            return i.score - this.score;
-        }
-        @Override
-        public String toString(){
-            return "score : " + score + ", calorie : " + calorie;
-        }
-    }
+	public static void C(int index, int scoreSum, int calorieSum, int[][] ingredients, boolean[] used) {
+		if(calorieSum > L) {
+			return;
+		}
+		if(index == N) {
+			answer = Math.max(answer, scoreSum);
+			return;
+		}
+		used[index] = true;
+		C(index+1, scoreSum + ingredients[index][0], calorieSum + ingredients[index][1], ingredients, used);
+		used[index] = false;
+		C(index+1, scoreSum, calorieSum, ingredients, used);
+	}
 }
