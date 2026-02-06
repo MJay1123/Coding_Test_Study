99b0bfb774acba60aa38775c1e5daf7e1dae6dab
Seo Min Jong <tjalswhd1@naver.com>
2026-02-06 15:48:35 +0900

    [D3] Title: 햄버거 다이어트, Time: 1,413 ms, Memory: 31,108 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -21,25 +21,23 @@ class Solution {
 				ingredients[i][1] = Integer.parseInt(st.nextToken());
 			}
 			answer = 0;
-			boolean[] used = new boolean[N];
-			C(0, 0, 0, ingredients, used);
+			for(int i=0; i<(1<<N); i++) {
+				int scoreSum = 0;
+				int calorieSum = 0;
+				for(int j=0; j<N; j++) {
+					if((i & (1 << j)) > 0) {
+						scoreSum += ingredients[j][0];
+						calorieSum += ingredients[j][1];
+					}
+				}
+				if(calorieSum <= L) {
+					answer = Math.max(answer,  scoreSum);
+				}
+			}
 			sb.append(answer).append(" ");
 			sb.append("\n");
 		}
 		bw.write(sb.toString());
 		bw.flush();
-	}
-	public static void C(int index, int scoreSum, int calorieSum, int[][] ingredients, boolean[] used) {
-		if(calorieSum > L) {
-			return;
-		}
-		if(index == N) {
-			answer = Math.max(answer, scoreSum);
-			return;
-		}
-		used[index] = true;
-		C(index+1, scoreSum + ingredients[index][0], calorieSum + ingredients[index][1], ingredients, used);
-		used[index] = false;
-		C(index+1, scoreSum, calorieSum, ingredients, used);
 	}
 }
