6639ec6a6a292c62d01a0f85545d1157def6c520
Seo Min Jong <tjalswhd1@naver.com>
2026-02-07 21:18:02 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 공통조상, Time: 156 ms, Memory: 32,896 KB -BaekjoonHub

---
+++ SWEA/D5/1248. ［S／W 문제해결 응용］ 3일차 － 공통조상/［S／W 문제해결 응용］ 3일차 － 공통조상.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+	static int V, E;
+	public static void main(String args[]) throws Exception	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for(int test_case = 1; test_case <= T; test_case++) {
+			sb.append("#").append(test_case).append(" ");
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			V = Integer.parseInt(st.nextToken());
+			E = Integer.parseInt(st.nextToken());
+			int num1 = Integer.parseInt(st.nextToken());
+			int num2 = Integer.parseInt(st.nextToken());
+			int[] tree = new int[V+1];
+			st = new StringTokenizer(br.readLine());
+			for(int i=0; i<E; i++) {
+				int parent = Integer.parseInt(st.nextToken());
+				int child = Integer.parseInt(st.nextToken());
+				tree[child] = parent;
+			}
+			int commonParent = getCommonParent(num1, num2, tree);
+			int subTreeSize = getSubTreeSize(commonParent, tree);
+            sb.append(commonParent).append(" ").append(subTreeSize).append("\n");
+		}
+		bw.write(sb.toString());
+        bw.flush();
+	}
+	public static int getCommonParent(int num1, int num2, int[] tree) {
+		boolean[] visited = new boolean[V+1];
+		int n = num1;
+		while(true) {
+			n = tree[n];
+			visited[n] = true;
+			if(n == 1) {
+				break;
+			}
+		}
+		n = num2;
+		while(true) {
+			n = tree[n];
+			if(visited[n]) {
+				return n;
+			}
+		}
+	}
+	public static int getSubTreeSize(int parent, int[] tree) {
+		int result = 1;
+		for(int i=1; i<=V; i++) {
+			if(tree[i] == parent) {
+				result += getSubTreeSize(i, tree);
+			}
+		}
+		return result;
+	}
+}
