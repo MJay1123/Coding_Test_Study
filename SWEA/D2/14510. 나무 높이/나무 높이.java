3eecb0596e3a5eaf30a166f9cd502917bb9903f4
Seo Min Jong <tjalswhd1@naver.com>
2026-02-12 23:50:20 +0900

    [D2] Title: 나무 높이, Time: 95 ms, Memory: 26,112 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -1,44 +1,53 @@
 import java.util.*;
 import java.io.*;
 
-class Solution {
-    static boolean finish = false;
-	public static void main(String args[]) throws Exception	{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+public class Solution {
+	static int N;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
-		for(int test_case = 1; test_case <= T; test_case++) {
-            int N = Integer.parseInt(br.readLine());
-            int[] trees = new int[N];
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int tallest = 0;
-            for(int i=0; i<N; i++){
-                trees[i] = Integer.parseInt(st.nextToken());
-                tallest = Math.max(tallest, trees[i]);
-            }
+		for (int testCase = 1; testCase <= T; testCase++) {
+			N = Integer.parseInt(br.readLine());
+			int[] trees = new int[N];
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int maxHeight = 0;
+			for (int i = 0; i < N; i++) {
+				trees[i] = Integer.parseInt(st.nextToken());
+				maxHeight = Math.max(maxHeight, trees[i]);
+			}
+			int oneCount = 0;
+			int twoCount = 0;
+			for (int i = 0; i < N; i++) {
+				int difference = maxHeight - trees[i];
+				if (difference % 2 == 1) {
+					oneCount++;
+				}
+				twoCount += difference / 2;
+			}
 			int answer = 0;
-            int oneCount = 0;
-            int twoCount = 0;
-            for(int i=0; i<N; i++){
-                twoCount  += (tallest - trees[i]) / 2;
-                oneCount += (tallest - trees[i]) % 2;
-            }
-            int temp = Math.min(oneCount, twoCount);
-            answer += temp * 2;
-            oneCount -= temp;
-            twoCount -= temp;
-            if(oneCount > 0){
-                answer += (oneCount - 1) * 2 + 1;
-            } else if(twoCount > 0){
-                answer += twoCount / 3 * 4;
-                if(twoCount % 3 > 0){
-                    answer += twoCount % 3 + 1;
-                }
-            }
-			StringBuilder sb = new StringBuilder();
-            sb.append("#").append(test_case).append(" ").append(answer).append("\n");
-            bw.write(sb.toString());
+			if (oneCount == twoCount) {
+				answer += 2 * twoCount;
+			} else if (oneCount > twoCount) {
+				answer += 2 * twoCount;
+				oneCount -= twoCount;
+				answer += 2 * (oneCount - 1) + 1;
+			} else {
+				answer += 2 * oneCount;
+				twoCount -= oneCount;
+				answer += (twoCount / 3) * 4;
+				twoCount %= 3;
+				if (twoCount == 1) {
+					answer += 2;
+				} else if (twoCount == 2) {
+					answer += 3;
+				}
+			}
+			sb.append("#").append(testCase).append(" ").append(answer).append("\n");
 		}
-        bw.flush();
+		bw.write(sb.toString());
+		bw.flush();
 	}
 }
