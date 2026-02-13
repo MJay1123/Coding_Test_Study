457c4bb3431b817652ef26e0b13a06419e6a0ded
Seo Min Jong <tjalswhd1@naver.com>
2026-02-13 09:05:58 +0900

    [Gold IV] Title: 암벽 등반, Time: 576 ms, Memory: 84328 KB -BaekjoonHub

--- 백준/Gold/2412. 암벽 등반/암벽 등반.java
+++ 백준/Gold/2412. 암벽 등반/암벽 등반.java
@@ -2,36 +2,9 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-	static class Point {
-		int x, y;
-
-		public Point(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-
-		@Override
-		public int hashCode() {
-			return Objects.hash(x, y);
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			Point p = (Point)obj;
-			return this.x == p.x && this.y == p.y;
-		}
-
-		@Override
-		public String toString() {
-			return "Point[x=" + x + ", y=" + y + "]";
-		}
-		
-	}
-
 	static int n, T;
-	static HashMap<Point, Integer> distanceMap = new HashMap<>();
+	static HashMap<Long, Integer> longMap = new HashMap<>();
 	static int answer;
-
 	public static void main(String args[]) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -39,12 +12,13 @@ class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		T = Integer.parseInt(st.nextToken());
-		distanceMap.put(new Point(0,0), 0);
+		longMap.put(0L, 0);
 		for (int i = 1; i <= n; i++) {
 			st = new StringTokenizer(br.readLine());
 			int x = Integer.parseInt(st.nextToken());
 			int y = Integer.parseInt(st.nextToken());
-			distanceMap.put(new Point(x, y), Integer.MAX_VALUE);
+			long yx = (long)y * 10000000L + x;
+			longMap.put(yx, Integer.MAX_VALUE);
 		}
 		answer = 1000000;
 		dijkstra();
@@ -57,30 +31,31 @@ class Main {
 	}
 
 	public static void dijkstra() {
-		Point start = new Point(0, 0);
-		PriorityQueue<Point> pq = new PriorityQueue<>(new Comparator<Point>() {
+		long start = 0L;
+		PriorityQueue<Long> pq = new PriorityQueue<>(new Comparator<Long>() {
 			@Override
-			public int compare(Point p1, Point p2) {
-				return distanceMap.get(p1) - distanceMap.get(p2);
+			public int compare(Long l1, Long l2) {
+				return longMap.get(l1) - longMap.get(l2);
 			}
 		});
 		pq.offer(start);
 		while(!pq.isEmpty()) {
-//			System.out.println(distanceMap);
-			Point currPoint = pq.poll();
-			if(currPoint.y == T) {
-				answer = Math.min(answer, distanceMap.get(currPoint));
+			long current = pq.poll();
+			long cx = current % 10000000L;
+			long cy = current / 10000000L;
+			if(cy == T) {
+				answer = Math.min(answer, longMap.get(current));
 			}
-			for(int x=currPoint.x-2; x<=currPoint.x+2; x++) {
-				for(int y=currPoint.y-2; y<=currPoint.y+2; y++) {
-					if(x == currPoint.x && y == currPoint.y) {
+			for(long x=cx-2; x<=cx+2; x++) {
+				for(long y=cy-2; y<=cy+2; y++) {
+					if(x == cx && y == cy) {
 						continue;
 					}
-					Point nextPoint = new Point(x, y);
-					if(distanceMap.containsKey(nextPoint)) {
-						if(distanceMap.get(currPoint) + 1 < distanceMap.get(nextPoint)) {
-							distanceMap.replace(nextPoint, distanceMap.get(currPoint) + 1);
-							pq.offer(nextPoint);
+					long next = y * 10000000L + x;
+					if(longMap.containsKey(next)) {
+						if(longMap.get(current) + 1 < longMap.get(next)) {
+							longMap.replace(next, longMap.get(current) + 1);
+							pq.offer(next);
 						}
 					}
 				}
