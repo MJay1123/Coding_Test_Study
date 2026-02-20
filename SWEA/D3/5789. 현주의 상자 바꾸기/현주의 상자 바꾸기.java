1a95451a8562145773659fb56843aea28f27de22
Seo Min Jong <tjalswhd1@naver.com>
2026-02-20 20:21:53 +0900

    [D3] Title: 현주의 상자 바꾸기, Time: 287 ms, Memory: 59,336 KB -BaekjoonHub

---
+++ SWEA/D3/5789. 현주의 상자 바꾸기/현주의 상자 바꾸기.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for(int testCase=1; testCase<=T; testCase++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int Q = Integer.parseInt(st.nextToken());
+			int[] arr = new int[N+1];
+			for(int i=1; i<=Q; i++) {
+				st = new StringTokenizer(br.readLine());
+				int L = Integer.parseInt(st.nextToken());
+				int R = Integer.parseInt(st.nextToken());
+				for(int j=L; j<=R; j++) {
+					arr[j] = i;
+				}
+			}
+			sb.append("#").append(testCase).append(" ");
+			for(int i=1; i<=N; i++) {
+				sb.append(arr[i]).append(" ");
+			}
+			sb.append("\n");
+		}
+		bw.write(sb.toString());
+		bw.flush();
+	}
+}
