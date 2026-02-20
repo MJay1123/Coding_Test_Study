a9e1a918fd4058007b05371e8ad3197835b223b6
Seo Min Jong <tjalswhd1@naver.com>
2026-02-20 20:26:39 +0900

    [D3] Title: 현주의 상자 바꾸기, Time: 668 ms, Memory: 76,392 KB -BaekjoonHub

--- SWEA/D3/5789. 현주의 상자 바꾸기/현주의 상자 바꾸기.java
+++ SWEA/D3/5789. 현주의 상자 바꾸기/현주의 상자 바꾸기.java
@@ -12,12 +12,21 @@ public class Solution {
 			int N = Integer.parseInt(st.nextToken());
 			int Q = Integer.parseInt(st.nextToken());
 			int[] arr = new int[N+1];
-			for(int i=1; i<=Q; i++) {
+			int[][] change = new int[Q][2];
+			for(int i=0; i<Q; i++) {
 				st = new StringTokenizer(br.readLine());
 				int L = Integer.parseInt(st.nextToken());
 				int R = Integer.parseInt(st.nextToken());
-				for(int j=L; j<=R; j++) {
-					arr[j] = i;
+				change[i][0] = L;
+				change[i][1] = R;
+			}
+			for(int r=Q-1; r>=0; r--) {
+				int L = change[r][0];
+				int R = change[r][1];
+				for(int c=L; c<=R; c++) {
+					if(arr[c] == 0) {
+						arr[c] = r+1;
+					}
 				}
 			}
 			sb.append("#").append(testCase).append(" ");
