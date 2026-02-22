d081fe86d8ac2b26e725449942ac4b5836ade772
Seo Min Jong <tjalswhd1@naver.com>
2026-02-22 23:19:13 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 공통조상, Time: 167 ms, Memory: 33,388 KB -BaekjoonHub

--- SWEA/D5/1248. ［S／W 문제해결 응용］ 3일차 － 공통조상/［S／W 문제해결 응용］ 3일차 － 공통조상.java
+++ SWEA/D5/1248. ［S／W 문제해결 응용］ 3일차 － 공통조상/［S／W 문제해결 응용］ 3일차 － 공통조상.java
@@ -3,33 +3,33 @@ import java.io.*;
 
 class Solution {
 	static int V, E;
+	static int[] tree;
 	public static void main(String args[]) throws Exception	{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
 		for(int test_case = 1; test_case <= T; test_case++) {
-			sb.append("#").append(test_case).append(" ");
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			V = Integer.parseInt(st.nextToken());
 			E = Integer.parseInt(st.nextToken());
 			int num1 = Integer.parseInt(st.nextToken());
 			int num2 = Integer.parseInt(st.nextToken());
-			int[] tree = new int[V+1];
+			tree = new int[V+1];
 			st = new StringTokenizer(br.readLine());
 			for(int i=0; i<E; i++) {
 				int parent = Integer.parseInt(st.nextToken());
 				int child = Integer.parseInt(st.nextToken());
 				tree[child] = parent;
 			}
-			int commonParent = getCommonParent(num1, num2, tree);
-			int subTreeSize = getSubTreeSize(commonParent, tree);
-            sb.append(commonParent).append(" ").append(subTreeSize).append("\n");
+			int commonParent = getCommonParent(num1, num2);
+			int subTreeSize = getSubTreeSize(commonParent);
+			sb.append("#").append(test_case).append(" ").append(commonParent).append(" ").append(subTreeSize).append("\n");
 		}
 		bw.write(sb.toString());
         bw.flush();
 	}
-	public static int getCommonParent(int num1, int num2, int[] tree) {
+	public static int getCommonParent(int num1, int num2) {
 		boolean[] visited = new boolean[V+1];
 		int n = num1;
 		while(true) {
@@ -47,11 +47,11 @@ class Solution {
 			}
 		}
 	}
-	public static int getSubTreeSize(int parent, int[] tree) {
+	public static int getSubTreeSize(int parent) {
 		int result = 1;
 		for(int i=1; i<=V; i++) {
 			if(tree[i] == parent) {
-				result += getSubTreeSize(i, tree);
+				result += getSubTreeSize(i);
 			}
 		}
 		return result;
