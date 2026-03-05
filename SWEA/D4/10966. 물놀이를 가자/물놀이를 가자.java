42d39ac6f322dca068b7e9955afedb63945d64ad
Seo Min Jong <tjalswhd1@naver.com>
2026-03-05 23:43:22 +0900

    [D4] Title: 물놀이를 가자, Time: 1,497 ms, Memory: 169,652 KB -BaekjoonHub

--- SWEA/D4/10966. 물놀이를 가자/물놀이를 가자.java
+++ SWEA/D4/10966. 물놀이를 가자/물놀이를 가자.java
@@ -1,85 +1,67 @@
 import java.util.*;
 import java.io.*;
 
-class Solution {
-	public static void main(String args[]) throws Exception	{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
+public class Solution {
+	static int N, M;
+	static char[][] map;
+	static int[][] countMap;
+	static int[][] around = {{1,0},{0,1},{-1,0},{0,-1}};
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
-		for(int test_case = 1; test_case <= T; test_case++) {
-			sb.append("#").append(test_case).append(" ");
+		for(int testCase=1; testCase<=T; testCase++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			int M = Integer.parseInt(st.nextToken());
-			char[][] map = new char[N][M];
-			List<int[]> waterList = new ArrayList<>();
-			List<int[]> landList = new ArrayList<>();
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			map = new char[N][M];
 			for(int r=0; r<N; r++) {
-				String str = br.readLine();
+				String temp = br.readLine();
 				for(int c=0; c<M; c++) {
-					map[r][c] = str.charAt(c);
-					if(map[r][c] == 'W') {
-						waterList.add(new int[] {r, c});
-					} else {
-						landList.add(new int[] {r, c});
-					}
+					map[r][c] = temp.charAt(c); 
 				}
 			}
-			int[][] distance = new int[N][M];
-			int INF = N * M;
-			for(int r=0; r<N; r++) {
-				Arrays.fill(distance[r], INF);
-			}
-			BFS(map, distance, waterList);
-            int answer = 0;
-            for(int[] l : landList) {
-            	answer += distance[l[0]][l[1]];
-            }
-            sb.append(answer).append("\n");
+			int answer = BFS();
+			sb.append("#").append(testCase).append(" ").append(answer).append("\n");
 		}
 		bw.write(sb.toString());
-        bw.flush();
+		bw.flush();
 	}
-	public static boolean checkRange(int r, int c, char[][] map) {
-		return r >= 0 && r < map.length && c >= 0 && c < map[0].length;
-	}
-	public static void BFS(char[][] map, int[][] distance, List<int[]> waterList) {
-		int[][] move = {{1,0},{0,1},{-1,0},{0,-1}};
-		Queue<Integer> rQueue = new ArrayDeque<>();
-		Queue<Integer> cQueue = new ArrayDeque<>();
-		for(int[] w : waterList) {
-			int r = w[0];
-			int c = w[1];
-			distance[r][c] = 0;
-			boolean temp = false;
-			for(int m=0; m<4; m++) {
-				int nr = r + move[m][0];
-				int nc = c + move[m][1];
-				if(checkRange(nr, nc, map) && map[nr][nc] == 'L') {
-					temp = true;
-					break;
+	public static int BFS() {
+		int count = 0;
+		countMap = new int[N][M];
+		for(int r=0; r<N; r++) {
+			Arrays.fill(countMap[r], -1);
+		}
+		Queue<Integer> rQueue = new LinkedList<>();
+		Queue<Integer> cQueue = new LinkedList<>();
+		for(int r=0; r<N; r++) {
+			for(int c=0; c<M; c++) {
+				if(map[r][c] == 'W') {
+					rQueue.offer(r);
+					cQueue.offer(c);
+					countMap[r][c] = 0;
 				}
-			}
-			if(temp) {
-				rQueue.offer(r);
-				cQueue.offer(c);
 			}
 		}
 		while(!rQueue.isEmpty()) {
 			int r = rQueue.poll();
 			int c = cQueue.poll();
-			for(int m=0; m<4; m++) {
-				int nr = r + move[m][0];
-				int nc = c + move[m][1];
-				if(checkRange(nr, nc, map)){
-					if(distance[nr][nc] != 0 && distance[nr][nc] > distance[r][c] + 1) {
-						distance[nr][nc] = distance[r][c] + 1;
-						rQueue.offer(nr);
-						cQueue.offer(nc);
-					}
+			for(int i=0; i<4; i++) {
+				int nr = r + around[i][0];
+				int nc = c + around[i][1];
+				if(checkRange(nr, nc) && countMap[nr][nc] == -1) {
+					rQueue.offer(nr);
+					cQueue.offer(nc);
+					countMap[nr][nc] = countMap[r][c] + 1;
+					count += countMap[nr][nc];
 				}
 			}
 		}
+		return count;
+	}
+	public static boolean checkRange(int r, int c) {
+		return r >= 0 && r < N && c >= 0 && c < M;
 	}
 }
