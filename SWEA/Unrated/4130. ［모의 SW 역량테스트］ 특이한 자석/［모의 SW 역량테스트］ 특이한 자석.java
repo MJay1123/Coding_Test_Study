6fd33e1fae106d816a38974a5aef4ba94ae96380
Seo Min Jong <tjalswhd1@naver.com>
2026-03-08 23:48:48 +0900

    [Unrated] Title: [모의 SW 역량테스트] 특이한 자석, Time: 83 ms, Memory: 25,728 KB -BaekjoonHub

--- SWEA/Unrated/4130. ［모의 SW 역량테스트］ 특이한 자석/［모의 SW 역량테스트］ 특이한 자석.java
+++ SWEA/Unrated/4130. ［모의 SW 역량테스트］ 특이한 자석/［모의 SW 역량테스트］ 특이한 자석.java
@@ -1,84 +1,60 @@
 import java.util.*;
 import java.io.*;
 
-class Solution {
-	public static void main(String args[]) throws Exception	{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
+public class Solution {
+	static int K;
+	static int[][] magnets;
+	static boolean[] visited;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
-		for(int test_case = 1; test_case <= T; test_case++) {
-			sb.append("#").append(test_case).append(" ");
-			int K = Integer.parseInt(br.readLine());
-			int[][] magnets = new int[5][8];
+		for(int testCase=1; testCase<=T; testCase++) {
+			K = Integer.parseInt(br.readLine());
+			magnets = new int[5][8];
 			for(int i=1; i<=4; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
 				for(int j=0; j<8; j++) {
-					magnets[i][j] = Integer.parseInt(st.nextToken());	// N : 0, S : 1
+					magnets[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
-			for(int k=0; k<K; k++) {
+			for(int i=0; i<K; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
 				int magnetNum = Integer.parseInt(st.nextToken());
-				int direction = Integer.parseInt(st.nextToken());	// 1 : 시계방향, -1 : 반시계방향
-				int[] rotation = new int[5];
-				rotation[magnetNum] = direction;
-				// 오른쪽 자석들의 맞물리는 극 판별
-				for(int i=magnetNum+1; i<=4; i++) {
-					int indexR = magnets[0][i];
-					int indexL = magnets[0][i-1];
-					if(magnets[i-1][(indexL + 2)%8] == magnets[i][(indexR+6)%8]) {
-						break;
-					}
-					rotation[i] = -rotation[i-1];
-				}
-				// 왼쪽 자석들의 맞물리는 극 판별
-				for(int i=magnetNum-1; i>=1; i--) {
-					int indexL = magnets[0][i];
-					int indexR = magnets[0][i+1];
-					if(magnets[i][(indexL+2)%8] == magnets[i+1][(indexR+6)%8]) {
-						break;
-					}
-					rotation[i] = -rotation[i+1];
-				}
-				// 자석들 회전
-				for(int i=1; i<=4; i++) {
-					if(rotation[i] != 0) {
-						rotate(magnets, i, rotation[i]);
-					}
-				}
+				int direction = Integer.parseInt(st.nextToken());
+				visited = new boolean[5];
+				rotate(magnetNum, direction);
 			}
 			int answer = 0;
 			for(int i=1; i<=4; i++) {
-				int index = magnets[0][i];
-				if(magnets[i][index] == 1) {
-					answer += (int)Math.pow(2, i-1);
-				}
+				answer += magnets[i][0] * Math.pow(2, i-1);
 			}
-            sb.append(answer).append("\n");
+			sb.append("#").append(testCase).append(" ").append(answer).append("\n");
 		}
 		bw.write(sb.toString());
-        bw.flush();
+		bw.flush();
 	}
-	public static int[] getRotation(int magnetNum, int direction) {
-		int[] rotation = new int[5];
-		if(magnetNum % 2 == 0) {
-			rotation[0] = direction;
-			rotation[2] = direction;
-			rotation[1] = -direction;
-			rotation[3] = -direction;
+	public static void rotate(int num, int direction) {
+		visited[num] = true;
+		if(num + 1 <= 4 && magnets[num][2] != magnets[num+1][6] && !visited[num+1]) {
+			rotate(num+1, -direction);
+		}
+		if(num - 1 >= 1 && magnets[num][6] != magnets[num-1][2] && !visited[num-1]) {
+			rotate(num-1, -direction);
+		}
+		if(direction == 1) {
+			int temp = magnets[num][7];
+			for(int i=7; i>=1; i--) {
+				magnets[num][i] = magnets[num][i-1];
+			}
+			magnets[num][0] = temp;
 		} else {
-			rotation[0] = -direction;
-			rotation[2] = -direction;
-			rotation[1] = direction;
-			rotation[3] = direction;
+			int temp = magnets[num][0];
+			for(int i=0; i<7; i++) {
+				magnets[num][i] = magnets[num][i+1];
+			}
+			magnets[num][7] = temp;
 		}
-		return rotation;
-	}
-	public static void rotate(int[][] magnets, int magnetNum, int direction) {
-		if(direction == 0) {
-			return;
-		}
-		magnets[0][magnetNum] = (magnets[0][magnetNum] - direction + 8) % 8;
 	}
 }
