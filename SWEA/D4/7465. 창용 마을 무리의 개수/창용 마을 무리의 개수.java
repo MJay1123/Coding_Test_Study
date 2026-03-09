c28f9c5e535a585c046f9106088a21c486782b3f
Seo Min Jong <tjalswhd1@naver.com>
2026-03-09 22:10:45 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 108 ms, Memory: 28,416 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -1,46 +1,43 @@
 import java.util.*;
 import java.io.*;
 
-class Solution {
-	public static void main(String args[]) throws Exception	{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
+public class Solution {
+	static int N, M;
+	static int[] arr;
+	static boolean[] visited;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
-		for(int test_case = 1; test_case <= T; test_case++) {
-			sb.append("#").append(test_case).append(" ");
+		for(int testCase=1; testCase<=T; testCase++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			int M = Integer.parseInt(st.nextToken());
-			int[] arr = new int[N+1];
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			arr = new int[N+1];
 			for(int i=0; i<M; i++) {
 				st = new StringTokenizer(br.readLine());
 				int a = Integer.parseInt(st.nextToken());
 				int b = Integer.parseInt(st.nextToken());
-				if(getRoot(a, arr) == getRoot(b, arr)) {
-					continue;
-				} else {
-					arr[getRoot(b, arr)] = a;
+				if(getRoot(a) != getRoot(b)) {
+					arr[getRoot(b)] = a;
 				}
 			}
-            int answer = 0;
-            for(int i=1; i<=N; i++) {
-            	if(arr[i] == 0) {
-            		answer++;
-            	}
-            }
-            sb.append(answer).append("\n");
-           
+			int answer = 0;
+			for(int i=1; i<=N; i++) {
+				if(arr[i] == 0) {
+					answer++;
+				}
+			}
+			sb.append("#").append(testCase).append(" ").append(answer).append("\n");
 		}
 		bw.write(sb.toString());
-        bw.flush();
+		bw.flush();
 	}
-	public static int getRoot(int num, int[] arr) {
-		while(true) {
-			if(arr[num] == 0) {
-				return num;
-			}
-			num = arr[num];
+	public static int getRoot(int num) {
+		if(arr[num] == 0) {
+			return num;
 		}
+		return getRoot(arr[num]);
 	}
 }
