1d373cb239b061055dd6448022904f77ec26fc7c
Seo Min Jong <tjalswhd1@naver.com>
2026-03-23 22:27:00 +0900

    [Gold IV] Title: 팰린드롬?, Time: 1564 ms, Memory: 214424 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int N, M;
+	static int[] numbers;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		N = Integer.parseInt(br.readLine());
+		numbers = new int[N+1];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=1; i<=N; i++) {
+			numbers[i] = Integer.parseInt(st.nextToken());
+		}
+		M = Integer.parseInt(br.readLine());
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int S = Integer.parseInt(st.nextToken());
+			int E = Integer.parseInt(st.nextToken());
+			if(isPalendrome(S, E)) {
+				bw.write(1 + "\n");
+			} else {
+				bw.write(0 + "\n");
+			}
+		}
+		bw.flush();
+	}
+	public static boolean isPalendrome(int S, int E) {
+		int left = S;
+		int right = E;
+		while(left <= right) {
+			if(numbers[left] != numbers[right]) {
+				return false;
+			}
+			left++;
+			right--;
+		}
+		return true;
+	}
+}
