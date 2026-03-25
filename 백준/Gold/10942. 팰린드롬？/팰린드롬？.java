c10b1197fb08244cb0e321963d7b2cae971906fa
Seo Min Jong <tjalswhd1@naver.com>
2026-03-25 17:17:18 +0900

    [Gold IV] Title: 팰린드롬?, Time: 660 ms, Memory: 214248 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -4,49 +4,45 @@ import java.io.*;
 public class Main {
 	static int N, M;
 	static int[] numbers;
-	static boolean[][] dp;
+	static int[] memo;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
 		N = Integer.parseInt(br.readLine());
 		numbers = new int[N+1];
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=1; i<=N; i++) {
 			numbers[i] = Integer.parseInt(st.nextToken());
 		}
-		dp = new boolean[N+1][N+1];
-		for(int i=1; i<=N; i++) {
-			for(int d=0; i-d>=1 && i+d<=N; d++) {
-				if(numbers[i-d] == numbers[i+d] && (d==0 || dp[i-d+1][i+d-1])) {
-					dp[i-d][i+d] = true;
-				} else {
-					break;
-				}
-			}
-		}
-		for(int i=1; i<=N-1; i++) {
-			for(int d=0; i-d>=1 && i+1+d<=N; d++) {
-				if(numbers[i-d] == numbers[i+1+d] && (d == 0 || dp[i-d+1][i+d])) {
-					dp[i-d][i+1+d] = true;
-				} else {
-					break;
-				}
-			}
-		}
+		memo = new int[N*2 + 1];
 		M = Integer.parseInt(br.readLine());
 		for(int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int S = Integer.parseInt(st.nextToken());
 			int E = Integer.parseInt(st.nextToken());
-			if(dp[S][E]) {
-				sb.append(1);
+			if(isPalendrome(S, E)) {
+				bw.write(1 + "\n");
 			} else {
-				sb.append(0);
+				bw.write(0 + "\n");
 			}
-			sb.append("\n");
 		}
-		bw.write(sb.toString());
 		bw.flush();
+	}
+	public static boolean isPalendrome(int S, int E) {
+		if((E-S) <= memo[S+E]) {
+			return true;
+		} else {
+			int left = S;
+			int right = E;
+			while(left < right) {
+				if(numbers[left] != numbers[right]) {
+					return false;
+				}
+				left++;
+				right--;
+			}
+			memo[S+E] = E-S;
+			return true;
+		}
 	}
 }
