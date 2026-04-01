e51f4fd7589ec6af023d1917ee3f4bb98ddc0976
Seo Min Jong <tjalswhd1@naver.com>
2026-04-01 12:00:06 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 2268 ms, Memory: 334564 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -0,0 +1,95 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int N, M;
+//	static List<Connection>[] connections;
+	static Road[] roads;
+	static class Road implements Comparable<Road> {
+		int num1;
+		int num2;
+		int cost;
+		public Road(int num1, int num2, int cost) {
+			this.num1 = num1;
+			this.num2 = num2;
+			this.cost = cost;
+		}
+		@Override
+		public int compareTo(Road r) {
+			return this.cost - r.cost;
+		}
+	}
+	static class Connection implements Comparable<Connection> {
+		int num;
+		int cost;
+		public Connection(int num, int cost) {
+			this.num = num;
+			this.cost = cost;
+		}
+		@Override
+		public int compareTo(Connection c) {
+			return this.cost - c.cost;
+		}
+	}
+	static int[] parents; 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+//		connections = new List[N+1];
+		roads = new Road[M];
+		parents = new int[N+1];
+		for(int i=1; i<=N; i++) {
+//			connections[i] = new ArrayList<>();
+			parents[i] = i;
+		}
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			int C = Integer.parseInt(st.nextToken());
+//			connections[A].add(new Connection(B, C));
+//			connections[B].add(new Connection(A, C));
+			roads[i] = new Road(A, B, C);
+		}
+		Arrays.sort(roads);
+		int roadCount = 0;
+		int groupCount = N;
+		int costSum = 0;
+		for(int i=0; i<M; i++) {
+			if(groupCount == 2) {
+				break;
+			}
+			Road r = roads[i];
+			int num1 = r.num1;
+			int num2 = r.num2;
+			int cost = r.cost;
+			if(union(num1, num2)) {
+				costSum += cost;
+				roadCount++;
+				groupCount--;
+			}
+		}
+		int answer = costSum;
+		bw.write(answer + "\n");
+		bw.flush();
+	}
+	public static int find(int num) {
+		if(parents[num] == num) {
+			return num;
+		}
+		parents[num] = find(parents[num]);
+		return parents[num];
+	}
+	public static boolean union(int num1, int num2) {
+		int root1 = find(num1);
+		int root2 = find(num2);
+		if(root1 == root2) {
+			return false;
+		}
+		parents[root1] = root2;
+		return true;
+	}
+}
