9f1702bbccfd42dba468b5756fa636c58e9b2878
Seo Min Jong <tjalswhd1@naver.com>
2026-04-02 12:56:01 +0900

    [Platinum V] Title: 이항 계수와 쿼리, Time: 812 ms, Memory: 487152 KB -BaekjoonHub

---
+++ 백준/Platinum/13977. 이항 계수와 쿼리/이항 계수와 쿼리.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int M, N, K;
+	static long MOD = 1000000007;
+	static long[] factorials = new long[40000001];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		M = Integer.parseInt(br.readLine());
+		for(int i=0; i<M; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());			
+			N = Integer.parseInt(st.nextToken());
+			K = Integer.parseInt(st.nextToken());
+			long up = factorial(N);
+			long down1 = square(factorial(K), MOD-2);
+			long down2 = square(factorial(N-K), MOD-2);
+			long answer = up * down1 % MOD * down2 % MOD;
+			sb.append(answer).append("\n");
+		}
+		bw.write(sb.toString());
+		bw.flush();
+	}
+	public static long square(long a, long b) {
+		if(b == 0) {
+			return 1;
+		}
+		long temp = square(a, b / 2);
+		if(b % 2 == 0) {
+			return temp * temp % MOD;
+		} else {
+			return temp * temp % MOD * a % MOD;
+		}
+	}
+	public static long factorial(int n) {
+		if(factorials[n] == 0) {
+			if(n == 0) {
+				factorials[0] = 1;
+			} else {
+				factorials[n] = factorial(n-1) * n % MOD;				
+			}
+		}
+		return factorials[n];
+	}
+}
