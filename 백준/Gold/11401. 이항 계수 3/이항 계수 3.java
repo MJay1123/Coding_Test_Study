b5e1b564d8a25802110230d8ba2d2feef898c23b
Seo Min Jong <tjalswhd1@naver.com>
2026-04-02 12:30:56 +0900

    [Gold I] Title: 이항 계수 3, Time: 264 ms, Memory: 143792 KB -BaekjoonHub

---
+++ 백준/Gold/11401. 이항 계수 3/이항 계수 3.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int N, K;
+	static long MOD = 1000000007;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		long up = factorial(N);
+		long down1 = square(factorial(K), MOD-2);
+		long down2 = square(factorial(N-K), MOD-2);
+		long answer = up * down1 % MOD * down2 % MOD;
+		bw.write(answer + "\n");
+		bw.flush();
+	}
+	public static long square(long a, long b) {
+		if(b == 0) {
+			return 1;
+		}
+		long temp = square(a, b/2);
+		if(b % 2 == 0) {
+			return (temp * temp) % MOD;
+		} else {
+			return ((temp * temp % MOD) * a) % MOD;
+		}
+	}
+	public static long factorial(long n) {
+		if(n == 0) {
+			return 1;
+		}
+		return factorial(n-1) * n % MOD;
+	}
+}
