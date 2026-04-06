b7caf3740c3ea2ee07ce5638260ffd94d0779ee7
Seo Min Jong <tjalswhd1@naver.com>
2026-04-06 17:42:21 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 82 ms, Memory: 25,984 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -1,42 +1,39 @@
 import java.util.*;
 import java.io.*;
 
-class Solution {
-	static int answer;
-	public static void main(String args[]) throws Exception	{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
+public class Solution {
+	static int[] tickets = new int[4];
+	static int[] days = new int[13];
+	static int MAX = 3000 * 365;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
-		for(int test_case = 1; test_case <= T; test_case++) {
+		for(int testCase=1; testCase<=T; testCase++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int[] prices = new int[4];
 			for(int i=0; i<4; i++) {
-				prices[i] = Integer.parseInt(st.nextToken());
+				tickets[i] = Integer.parseInt(st.nextToken());
 			}
-			int[] days = new int[13];
 			st = new StringTokenizer(br.readLine());
 			for(int i=1; i<=12; i++) {
 				days[i] = Integer.parseInt(st.nextToken());
-				days[0] += days[i];
 			}
-			answer = 3000 * 30 * 12;
-			if(days[0] > 0) {
-				C(1, 0, prices, days);
+			int[] dp = new int[13];
+			Arrays.fill(dp, MAX);
+			dp[12] = tickets[3];
+			dp[0] = 0;
+			for(int month=1; month<=12; month++) {
+				if(month - 3 >= 0) {
+					dp[month] = Math.min(dp[month], dp[month-3] + tickets[2]);
+				}
+				dp[month] = Math.min(dp[month], dp[month-1] + tickets[1]);
+				dp[month] = Math.min(dp[month], dp[month-1] + days[month] * tickets[0]);
 			}
-            sb.append("#").append(test_case).append(" ").append(answer).append("\n");
+			int answer = dp[12];
+			sb.append("#").append(testCase).append(" ").append(answer).append("\n");
 		}
 		bw.write(sb.toString());
-        bw.flush();
-	}
-	public static void C(int month, int money, int[] prices, int[] days) {
-		if(month > 12) {
-			answer = Math.min(answer, money);
-			return;
-		}
-		C(month+1, money + prices[0] * days[month], prices, days);
-		C(month+1, money + prices[1], prices, days);
-		C(month+3, money + prices[2], prices, days);
-		C(month+12, money + prices[3], prices, days);
+		bw.flush();
 	}
 }
