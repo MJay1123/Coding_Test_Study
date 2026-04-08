60946acc3e729f6f7247f0752a6faca8d7bfd337
Seo Min Jong <tjalswhd1@naver.com>
2026-04-08 09:37:05 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보호 필름, Time: 3,826 ms, Memory: 28,544 KB -BaekjoonHub

---
+++ SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
@@ -0,0 +1,68 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+	static int D, W, K;
+	static int[] cells;
+	static int answer;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for(int testCase=1; testCase<=T; testCase++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			D = Integer.parseInt(st.nextToken());
+			W = Integer.parseInt(st.nextToken());
+			K = Integer.parseInt(st.nextToken());
+			cells = new int[D];
+			for(int r=0; r<D; r++) {
+				st = new StringTokenizer(br.readLine());
+				for(int c=0; c<W; c++) {
+					int num = Integer.parseInt(st.nextToken());
+					cells[r] = cells[r] | (num << c);
+				}
+			}
+			answer = 100000;
+			combination(0, 0);
+			sb.append("#").append(testCase).append(" ").append(answer).append("\n");
+		}
+		bw.write(sb.toString());
+		bw.flush();
+	}
+	public static void combination(int r, int count) {
+		if(r == D) {
+			if(check()) {
+				answer = Math.min(answer, count);
+			}
+			return;
+		}
+		int bitO = cells[r];
+		int bitA = 0;
+		int bitB = (1 << W) - 1;
+		combination(r+1, count);
+		cells[r] = bitA;
+		combination(r+1, count+1);
+		cells[r] = bitB;
+		combination(r+1, count+1);
+		cells[r] = bitO;
+	}
+	public static boolean check() {
+		for(int c=0; c<W; c++) {
+			int max = 1;
+			int count = 1;
+			for(int r=1; r<D; r++) {
+				if((cells[r] & (1 << c)) == (cells[r-1] & (1 << c))) {
+					count++;
+				} else {
+					count = 1;
+				}
+				max = Math.max(max, count);
+			}
+			if(max < K) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
