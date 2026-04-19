f15a31d5d9c8c57631754428a65c4785818fe2ac
Seo Min Jong <tjalswhd1@naver.com>
2026-04-19 15:58:38 +0900

    [D4] Title: 초콜릿과 건포도, Time: 4,423 ms, Memory: 178,732 KB -BaekjoonHub

---
+++ SWEA/D4/9282. 초콜릿과 건포도/초콜릿과 건포도.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+	static int n, m;
+	static int[][] chocolate;
+	static long[][][][] dp;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		int TC = Integer.parseInt(br.readLine());
+		for(int testCase=1; testCase<=TC; testCase++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			n = Integer.parseInt(st.nextToken());
+			m = Integer.parseInt(st.nextToken());
+			chocolate = new int[n][m];
+			for(int r=0; r<n; r++) {
+				st = new StringTokenizer(br.readLine());
+				for(int c=0; c<m; c++) {
+					chocolate[r][c] = Integer.parseInt(st.nextToken());
+				}
+			}
+			dp = new long[n][m][n][m];
+			long answer = getDP(0, 0, n-1, m-1);
+			sb.append(String.format("#%d %d\n", testCase, answer));
+		}
+		bw.write(sb.toString());
+		bw.flush();
+	}
+	public static long getDP(int sr, int sc, int er, int ec) {
+		if(sr == er && sc == ec) {
+			return 0;
+		} else {
+			if(dp[sr][sc][er][ec] == 0) {
+				long sum = 0;
+				for(int r=sr; r<=er; r++) {
+					for(int c=sc; c<=ec; c++) {
+						sum += chocolate[r][c];
+					}
+				}
+				long minimum = Long.MAX_VALUE;
+				for(int r=sr; r<er; r++) {
+					long result = getDP(sr, sc, r, ec) + getDP(r+1, sc, er, ec);
+					minimum = Math.min(minimum, sum + result);
+				}
+				for(int c=sc; c<ec; c++) {
+					long result = getDP(sr, sc, er, c) + getDP(sr, c+1, er, ec);
+					minimum = Math.min(minimum, sum + result);					
+				}
+				dp[sr][sc][er][ec] = minimum;
+			}
+		}
+		return dp[sr][sc][er][ec];
+	}
+}
+
