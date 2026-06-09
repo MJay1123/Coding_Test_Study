e33bc7f5c0072dbda8a3bc762eac7bfef1b98732
Seo Min Jong <tjalswhd1@naver.com>
2026-06-09 15:59:46 +0900

    [D2] Title: 달팽이 숫자, Time: 78 ms, Memory: 25,216 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
@@ -2,27 +2,29 @@ import java.util.*;
 import java.io.*;
 
 class Solution {
+    static int N;
+    static int[][] arr;
 	public static void main(String args[]) throws Exception	{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
         int[][] direction = {{0,1}, {1,0}, {0,-1}, {-1,0}};
 		for(int test_case = 1; test_case <= T; test_case++) {
-            int N = Integer.parseInt(br.readLine());
-            int[][] arr = new int[N][N];
+            N = Integer.parseInt(br.readLine());
+            arr = new int[N][N];
             int number = 1;
             int directionIndex = 0;
             int r = 0; 
             int c = 0;
             while(true){
-                if(!checkRange(r, c, N) || arr[r][c] != 0){
+                if(!checkRange(r, c) || arr[r][c] != 0){
                     break;
                 }
-                arr[r][c] = number;
-                number++;
+                arr[r][c] = number++;
                 int nr = r + direction[directionIndex][0];
                 int nc = c + direction[directionIndex][1];
-                if(!checkRange(nr, nc, N) || arr[nr][nc] != 0){
+                if(!checkRange(nr, nc) || arr[nr][nc] != 0){
                     directionIndex = (directionIndex + 1) % 4;
                     nr = r + direction[directionIndex][0];
                     nc = c + direction[directionIndex][1];
@@ -30,17 +32,18 @@ class Solution {
                 r = nr;
                 c = nc;
             }
-            bw.write("#" + test_case + "\n");
+            sb.append("#").append(test_case).append("\n");
             for(int i=0; i<N; i++){
                 for(int j=0; j<N; j++){
-                    bw.write(arr[i][j] + " ");
+                    sb.append(arr[i][j]).append(" ");
                 }
-                bw.write("\n");
+                sb.append("\n");
             }
 		}
+        bw.write(sb.toString());
         bw.flush();
 	}
-    public static boolean checkRange(int r, int c, int N){
+    public static boolean checkRange(int r, int c){
         return r >= 0 && r < N && c >= 0 && c < N;
     }
 }
