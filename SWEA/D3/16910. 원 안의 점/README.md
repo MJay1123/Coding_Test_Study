47e82c881e7db6d0e223cb1d2351d6c88f8d60b0
Seo Min Jong <tjalswhd1@naver.com>
2026-07-07 08:34:00 +0900

    [D3] Title: 원 안의 점, Time: 107 ms, Memory: 26,880 KB -BaekjoonHub

---
+++ SWEA/D3/16910. 원 안의 점/README.md
@@ -0,0 +1,15 @@
+# [D3] 원 안의 점 - 16910 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AYcllbDqUVgDFASR) 
+
+### 성능 요약
+
+메모리: 26,880 KB, 시간: 107 ms, 코드길이: 909 Bytes
+
+### 제출 일자
+
+2026-07-07 08:33
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/16910. 원 안의 점/원 안의 점.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+public class Solution {
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for(int testCase=1; testCase<=T; testCase++){
+            N = Integer.parseInt(br.readLine());
+            int answer = 0;
+            for(int x=-N; x<=N; x++){
+                for(int y=-N; y<=N; y++){
+                    if(x * x + y * y <= N * N){
+                        answer++;
+                    }
+                }
+            }
+            sb.append("#").append(testCase).append(" ").append(answer).append("\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
