4bb1d7e8d9b144633ced2abeae42023d7f5e06a3
Seo Min Jong <tjalswhd1@naver.com>
2026-07-07 08:35:03 +0900

    [D3] Title: 원 안의 점, Time: 95 ms, Memory: 27,008 KB -BaekjoonHub

--- SWEA/D3/16910. 원 안의 점/원 안의 점.java
+++ SWEA/D3/16910. 원 안의 점/원 안의 점.java
@@ -10,13 +10,14 @@ public class Solution {
         for(int testCase=1; testCase<=T; testCase++){
             N = Integer.parseInt(br.readLine());
             int answer = 0;
-            for(int x=-N; x<=N; x++){
-                for(int y=-N; y<=N; y++){
+            for(int x=1; x<=N; x++){
+                for(int y=1; y<=N; y++){
                     if(x * x + y * y <= N * N){
                         answer++;
                     }
                 }
             }
+            answer = answer * 4 + N * 4 + 1;
             sb.append("#").append(testCase).append(" ").append(answer).append("\n");
         }
         bw.write(sb.toString());
