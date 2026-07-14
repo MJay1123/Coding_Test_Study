74c9dfc0fe402029dbbede55631fd10b876dbe10
Seo Min Jong <tjalswhd1@naver.com>
2026-07-14 09:14:23 +0900

    [D3] Title: 부분 수열의 합, Time: 1,225 ms, Memory: 29,388 KB -BaekjoonHub

---
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+public class Solution {
+    static int N, K;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for(int tc=1; tc<=T; tc++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+            int[] arr = new int[N];
+            st = new StringTokenizer(br.readLine());
+            for(int i=0; i<N; i++){
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+            int answer = 0;
+            for(int bit=0; bit<(1 << N); bit++){
+                int sum = 0;
+                for(int d=0; d<N; d++){
+                    if((bit & (1 << d)) > 0){
+                        sum += arr[d];
+                        if(sum > K){
+                            break;
+                        }
+                    }
+                }
+                if(sum == K){
+                    answer++;
+                }
+            }
+            sb.append("#").append(tc).append(" ").append(answer).append("\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
