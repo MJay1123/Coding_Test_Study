5300c42de6e97cdf3f25aae038dd32ed19a046fc
Seo Min Jong <tjalswhd1@naver.com>
2026-07-25 16:54:04 +0900

    [Unrated] Title: [모의 SW 역량테스트] 탈주범 검거, Time: 109 ms, Memory: 32,384 KB -BaekjoonHub

--- SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
+++ SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
@@ -1,12 +1,11 @@
 import java.util.*;
 import java.io.*;
-
 public class Solution {
 	static int N, M, R, C, L;
-	static int[][] map;
-	static boolean[][] visited;
 	static int[][] around = {{1,0},{0,1},{-1,0},{0,-1}};
-	static int[][] connectedTo = {{}, {0,1,2,3},{0,2},{1,3},{1,2},{0,1},{0,3},{2,3}};
+	static int[][] connection = {{}, {0,1,2,3},{0,2},{1,3},{1,2},{0,1},{0,3},{2,3}};
+    static int[][] map;
+	static boolean[][] visited;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -33,45 +32,50 @@ public class Solution {
 		bw.write(sb.toString());
 		bw.flush();
 	}
-	public static boolean checkRange(int r, int c) {
+    public static int BFS(int startR, int startC){
+        int count = 0;
+        Queue<Integer> rQueue = new LinkedList<>();
+        Queue<Integer> cQueue = new LinkedList<>();
+        Queue<Integer> distQueue = new LinkedList<>();
+        rQueue.offer(startR);
+        cQueue.offer(startC);
+        distQueue.offer(1);
+        visited[startR][startC] = true;
+        count++;
+        while(!rQueue.isEmpty()){
+            int r = rQueue.poll();
+            int c = cQueue.poll();
+            int dist = distQueue.poll();
+            int pipeType = map[r][c];
+            int[] directions = connection[pipeType];
+            for(int i=0; i<directions.length; i++){
+                int direction = directions[i];
+                int nr = r + around[direction][0];
+                int nc = c + around[direction][1];
+             	if(checkRange(nr, nc) && !visited[nr][nc] && map[nr][nc] > 0 && dist + 1 <= L && canConnected(direction, map[nr][nc])){
+                    rQueue.offer(nr);
+                    cQueue.offer(nc);
+                    distQueue.offer(dist + 1);
+                    visited[nr][nc] = true;
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+	public static boolean canConnected(int direction, int nextPipe){
+        if(nextPipe == 1){
+            return true;
+        }
+        int[][] arr = {{2,4,7},{3,6,7},{2,5,6},{3,4,5}};
+        for(int pipe : arr[direction]){
+            if(nextPipe == pipe){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static boolean checkRange(int r, int c) {
 		return r >= 0 && r < N && c >= 0 && c < M;
-	}
-	public static int BFS(int startR, int startC){
-		int count = 0;
-		Queue<Integer> rQueue = new LinkedList<>();
-		Queue<Integer> cQueue = new LinkedList<>();
-		Queue<Integer> timeQueue = new LinkedList<>();
-		rQueue.offer(startR);
-		cQueue.offer(startC);
-		timeQueue.offer(1);
-		visited[startR][startC] = true;
-		count++;
-		while(!rQueue.isEmpty()) {
-			int r = rQueue.poll();
-			int c = cQueue.poll();
-			int time = timeQueue.poll();
-			if(time < L) {
-				for(int i : connectedTo[map[r][c]]) {
-					int nr = r + around[i][0];
-					int nc = c + around[i][1];
-					if(checkRange(nr, nc) && contains(connectedTo[map[nr][nc]], (i+2)%4) && !visited[nr][nc]) {
-						rQueue.offer(nr);
-						cQueue.offer(nc);
-						timeQueue.offer(time+1);
-						visited[nr][nc] = true;
-						count++;
-					}
-				}
-			}
-		}
-		return count;
-	}
-	public static boolean contains(int[] arr, int num) {
-		for(int i=0; i<arr.length; i++) {
-			if(arr[i] == num) {
-				return true;
-			}
-		}
-		return false;
 	}
 }
