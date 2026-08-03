d119db6c2094530deb37408f5a03549b32bb9aaa
Seo Min Jong <tjalswhd1@naver.com>
2026-08-04 00:18:55 +0900

    [level 2] Title: N-Queen, Time: 56.05 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -1,43 +1,33 @@
 import java.util.*;
 class Solution {
     static int answer = 0;
-    static int[][] move = {{-1,1},{-1,0},{-1,-1}};
+    static int N;
+    static boolean[][] board;
+    static boolean[] columns;
+    static boolean[][] diagonals;
     public int solution(int n) {
-        boolean[][] chessBoard = new boolean[n][n];
-        boolean[] usedColumn = new boolean[n];
-        placeInRow(chessBoard, 0);
+        N = n;
+        board = new boolean[N][N];
+        columns = new boolean[N];
+        diagonals = new boolean[2][2*N];
+        check(0);
         return answer;
     }
-    public void placeInRow(boolean[][] chessBoard, int r){
-        int n = chessBoard.length;
-        if(r == n){
+    public void check(int r){
+        if(r == N){
             answer++;
             return;
         }
-        for(int c=0; c<n; c++){
-            if(checkPreviousRows(r, c, chessBoard)){
-                chessBoard[r][c] = true;
-                placeInRow(chessBoard, r+1);
-                chessBoard[r][c] = false;
+        for(int c=0; c<N; c++){
+            if(!columns[c] && !diagonals[0][r+c] && !diagonals[1][r-c+N]){
+                columns[c] = true;
+                diagonals[0][r+c] = true;
+                diagonals[1][r-c+N] = true;
+                check(r+1);
+                columns[c] = false;
+                diagonals[0][r+c] = false;
+                diagonals[1][r-c+N] = false;
             }
         }
-    }
-    public boolean checkPreviousRows(int r, int c, boolean[][] chessBoard){
-        int n = chessBoard.length;
-        for(int i=0; i<3; i++){
-            for(int j=0; j<n; j++){
-                int nr = r + move[i][0] * j;
-                int nc = c + move[i][1] * j;
-                if(checkRange(nr, nc, n)){
-                    if(chessBoard[nr][nc]){
-                        return false;
-                    }
-                }
-            }
-        }
-        return true;
-    }
-    public boolean checkRange(int r, int c, int n){
-        return r >= 0 && r < n && c >= 0 && c < n;
     }
 }
--- 프로그래머스/2/12952. N－Queen/README.md
+++ 프로그래머스/2/12952. N－Queen/README.md
@@ -1,10 +1,10 @@
 # [level 2] N-Queen - 12952 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12952?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12952?language=java) 
 
 ### 성능 요약
 
-메모리: 9.34 MB, 시간: 972.87 ms
+메모리: 79.3 MB, 시간: 56.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 28일 13:52:27
+2026년 08월 04일 00:18:49
 
 ### 문제 설명
 
