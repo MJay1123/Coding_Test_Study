d3af0db49aeee820c7d63c1bf4f5a2b5f470ebd2
Seo Min Jong <tjalswhd1@naver.com>
2026-08-03 23:49:54 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 최적 경로, Time: 119 ms, Memory: 26,368 KB -BaekjoonHub

--- SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
+++ SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
@@ -1,63 +1,57 @@
 import java.util.*;
 import java.io.*;
-
 public class Solution {
-	static class Location {
-		int x;
-		int y;
-		public Location(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-	}
 	static int N;
-	static Location home;
-	static Location company;
-	static Location[] customers;
+    static int[] company;
+    static int[] home;
+    static int[][] customers;
 	static int answer;
-	static boolean[] visited;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
-		for(int testCase=1; testCase<=T; testCase++) {
+		for(int tc=1; tc<=T; tc++) {
 			N = Integer.parseInt(br.readLine());
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
-			company = new Location(x, y);
-			x = Integer.parseInt(st.nextToken());
-			y = Integer.parseInt(st.nextToken());
-			home = new Location(x, y);
-			customers = new Location[N];
+            company = new int[2];
+            company[0] = Integer.parseInt(st.nextToken());
+			company[1] = Integer.parseInt(st.nextToken());
+            home = new int[2];
+			home[0] = Integer.parseInt(st.nextToken());
+			home[1] = Integer.parseInt(st.nextToken());
+            customers = new int[N][2];
 			for(int i=0; i<N; i++) {
-				x = Integer.parseInt(st.nextToken());
-				y = Integer.parseInt(st.nextToken());
-				customers[i] = new Location(x, y); 
+				customers[i][0] = Integer.parseInt(st.nextToken());
+				customers[i][1] = Integer.parseInt(st.nextToken());
 			}
-			answer = 10000000;
-			visited = new boolean[N];
-			goToNext(0, 0, company);
-			sb.append("#").append(testCase).append(" ").append(answer).append("\n");
+			answer = 200 * (N + 1);
+			boolean[] visited = new boolean[N];
+			visit(0, company[0], company[1], visited, 0);
+			sb.append("#").append(tc).append(" ").append(answer).append("\n");
 		}
 		bw.write(sb.toString());
 		bw.flush();
 	}
-	public static void goToNext(int visitedCount, int distance, Location current) {
-		if(visitedCount == N) {
-			answer = Math.min(answer, distance + getDistance(current, home));
-			return;
-		}
-		for(int i=0; i<N; i++) {
-			if(!visited[i]) {
-				visited[i] = true;
-				goToNext(visitedCount+1, distance + getDistance(current, customers[i]), customers[i]);
-				visited[i] = false;
-			}
-		}
-	}
-	public static int getDistance(Location loc1, Location loc2) {
-		return Math.abs(loc1.x - loc2.x) + Math.abs(loc1.y - loc2.y); 
+    public static void visit(int distance, int x, int y, boolean[] visited, int visitCount){
+        if(distance + getDistance(x, y, home[0], home[1]) > answer){
+            return;
+        }
+        if(visitCount == N){
+            answer = Math.min(answer, distance + getDistance(x, y, home[0], home[1]));
+            return;
+        }
+        for(int i=0; i<N; i++){
+            if(!visited[i]){
+                int nx = customers[i][0];
+                int ny = customers[i][1];
+                visited[i] = true;
+                visit(distance+getDistance(x, y, nx, ny), nx, ny, visited, visitCount+1);
+                visited[i] = false;
+            }
+        }
+    }
+	public static int getDistance(int x1, int y1, int x2, int y2) {
+		return Math.abs(x1 - x2) + Math.abs(y1 - y2); 
 	}
 }
