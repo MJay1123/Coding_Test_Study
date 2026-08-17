6f9c6cf35c2c541ceb6b971a57346874f63e3f80
Seo Min Jong <tjalswhd1@naver.com>
2026-08-17 17:05:48 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 2.59 ms, Memory: 96.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,60 +1,63 @@
 import java.util.*;
 class Solution {
-    static class Process {
-        int number;
-        int request;
-        int length;
-        Integer start;
-        public Process(int number, int request, int length){
-            this.number = number;
-            this.request = request;
-            this.length = length;
-        }
-    }
-    static Process processing;
     public int solution(int[][] jobs) {
-        PriorityQueue<Process> waitingQueue = new PriorityQueue<>(new Comparator<Process>(){
+        PriorityQueue<Work> jobsQueue = new PriorityQueue<>(new Comparator<Work>(){
             @Override
-            public int compare(Process p1, Process p2){
-                return p1.request - p2.request;
+            public int compare(Work w1, Work w2){
+                return w1.requestTime - w2.requestTime;
+            }
+        });
+        PriorityQueue<Work> waitingQueue = new PriorityQueue<>(new Comparator<Work>(){
+            @Override
+            public int compare(Work w1, Work w2){
+                if(w1.processTime != w2.processTime){
+                    return w1.processTime - w2.processTime;
+                }
+                if(w1.requestTime != w2.requestTime){
+                    return w1.requestTime - w2.requestTime;
+                }
+                return w1.number - w2.number;
             }
         });
         for(int i=0; i<jobs.length; i++){
-            int s = jobs[i][0];
-            int l = jobs[i][1];
-            waitingQueue.offer(new Process(i, s, l));
+            jobsQueue.offer(new Work(i, jobs[i][0], jobs[i][1]));
         }
-        int answer = 0;
-        PriorityQueue<Process> dc = new PriorityQueue<>(new Comparator<Process>(){
-            @Override
-            public int compare(Process p1, Process p2){
-                if(p1.length != p2.length){
-                    return p1.length - p2.length;
-                }
-                if(p1.request != p2.request){
-                    return p1.request - p2.request;
-                }
-                return p1.number - p2.number;
-            }
-            
-        });
-        int time = 0;
+        int currentTime = 0;
+        int totalTime = 0;
+        Work work = null;
+
         while(true){
-            if(waitingQueue.isEmpty() && dc.isEmpty()){
+            if(waitingQueue.isEmpty() && jobsQueue.isEmpty()){
                 break;
             }
-            while(!waitingQueue.isEmpty() && waitingQueue.peek().request <= time){
-                dc.offer(waitingQueue.poll());
+            if(waitingQueue.isEmpty()){
+                if(!jobsQueue.isEmpty() && currentTime <= jobsQueue.peek().requestTime){
+                    currentTime = jobsQueue.peek().requestTime;
+                }
             }
-            if(!dc.isEmpty()){
-                processing = dc.poll();
-                time += processing.length;
-                answer += time - processing.request;
-            } else {
-                time++;
+            while(!jobsQueue.isEmpty() && jobsQueue.peek().requestTime <= currentTime){
+                waitingQueue.offer(jobsQueue.poll());
             }
+            work = waitingQueue.poll();
+            currentTime += work.processTime;
+            totalTime += (currentTime - work.requestTime);
         }
-        answer /= jobs.length;
+        int answer = totalTime / jobs.length;
         return answer;
+    }
+    class Work {
+        int number;
+        int requestTime;
+        int processTime;
+        public Work(int number, int requestTime, int processTime){
+            this.number = number;
+            this.requestTime = requestTime;
+            this.processTime = processTime;
+        }
+        
+        @Override
+        public String toString(){
+            return "Work[num : " + number + ", r : " + requestTime + ", p : " + processTime + "]";
+        }
     }
 }
