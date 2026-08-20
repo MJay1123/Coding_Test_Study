39f9808e7ba4e32d810d9320f54ebd67614f5699
Seo Min Jong <tjalswhd1@naver.com>
2026-08-20 14:24:55 +0900

    [D3] Title: 세제곱근을 찾아라, Time: 75 ms, Memory: 25,472 KB -BaekjoonHub

---
+++ SWEA/D3/5688. 세제곱근을 찾아라/세제곱근을 찾아라.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+public class Solution {
+    static long N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for(int tc=1; tc<=T; tc++){
+            N = Long.parseLong(br.readLine());
+            long answer = -1;
+            long low = 1;
+            long high = 1000000;
+            while(low <= high){
+                long middle = (low + high) / 2;
+                long result = middle * middle * middle;
+                if(result == N){
+                    answer = middle;
+                    break;
+                }
+                if(low == high){
+                    break;
+                }
+                if(result > N){
+                    high = middle;
+                } else if(result < N){
+                    low = middle + 1;
+                }
+            }
+            sb.append("#").append(tc).append(" ").append(answer).append("\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
