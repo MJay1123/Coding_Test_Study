31cdcffa13f3832b01ef3eccb5f3493412fed407
Seo Min Jong <tjalswhd1@naver.com>
2026-08-31 13:10:56 +0900

    [D3] Title: 이진수 표현, Time: 123 ms, Memory: 33,024 KB -BaekjoonHub

--- SWEA/D3/10726. 이진수 표현/이진수 표현.java
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.java
@@ -2,25 +2,32 @@ import java.util.*;
 import java.io.*;
 
 class Solution {
-	public static void main(String args[]) throws Exception	{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
-		int TC = Integer.parseInt(br.readLine());
-		for(int test_case = 1; test_case <= TC; test_case++) {
-			sb.append("#").append(test_case).append(" ");
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			int M = Integer.parseInt(st.nextToken());
-			int lastNBit = (1 << N) -1 ;
-			if((lastNBit & M) == lastNBit) {
-				sb.append("ON");
-			} else {
-				sb.append("OFF");
-			}
+        int T = Integer.parseInt(br.readLine());
+        for(int tc=1; tc<=T; tc++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            boolean answer = true;
+            for(int i=0; i<N; i++){
+                if((M & 1) == 0){
+                    answer = false;
+                    break;
+                }
+                M = (M >> 1);
+            }
+            sb.append("#").append(tc).append(" ");
+            if(answer){
+                sb.append("ON");
+            } else {
+                sb.append("OFF");
+            }
             sb.append("\n");
-		}
-		bw.write(sb.toString());
-        bw.flush();
-	}
+        }
+        bw.write(sb.toString());
+    	bw.flush();
+    }
 }
